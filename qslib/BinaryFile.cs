a37ca333fd27dd743ef6ba8a035a39f17b981b16
AlexM-Dev <mac0017@drouinsc.vic.edu.au>
2018-03-01 15:23:27 +1100

    Add project files.

---
+++ qslib/BinaryFile.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace qslib {
+    public class BinaryFile {
+        public static void Save<T>(string fileName, T obj) {
+            using (Stream stream = System.IO.File.Open(fileName, FileMode.Create)) {
+                var binary = new BinaryFormatter();
+
+                binary.Serialize(stream, obj);
+            }
+        }
+        public static T Load<T>(string fileName) {
+            using (Stream stream = System.IO.File.Open(fileName, FileMode.Open)) {
+                var binary = new BinaryFormatter();
+
+                return (T)binary.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ qslib/SerializableObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace qslib {
+    /*
+     * A class for the simplification of serializing/deserializing custom
+     * objects.
+     * Alex M. 2018.
+     */
+    [Serializable()]
+    public abstract class SerializableObject {
+        public SerializableObject() {
+
+        }
+
+        public SerializableObject(
+            SerializationInfo info, StreamingContext context) {
+
+            foreach (PropertyInfo property in this.GetType().GetProperties()) {
+                property.SetValue(this, info.GetValue(property.Name,
+                    property.GetType()));
+            }
+        }
+        public void GetSerializationData(
+            SerializationInfo info, StreamingContext context) {
+            foreach (PropertyInfo property in this.GetType().GetProperties()) {
+                info.AddValue(property.Name, property.GetValue(this));
+            }
+        }
+
+        public void Save(string fileName) {
+            BinaryFile.Save(fileName, this);
+        }
+        public static SerializableObject Load(string fileName) {
+            return BinaryFile.Load<SerializableObject>(fileName);
+        }
+    }
+}
